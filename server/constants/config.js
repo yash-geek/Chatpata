bac3eb28a041e56564b26662fac8bcea8cb3a7f1
yash-geek <yashgautam8738@gmail.com>
2025-07-07 22:12:08 +0530

    Deployment commit 1

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:4173'],
+    origin: ['http://localhost:5173', 'http://localhost:4173','https://www.chatapata.xyz', 'https://chatpata-five.vercel.app'],
     credentials: true,
     methods: ["GET","POST","PUT","DELETE"]
 }
